4aa96cb70afb52713b3a44fdcabe82c20d22a04e
krista1 <wf.open@bk.ru>
2019-09-09 19:12:46 +0300

    Test1
    v1.0

---
+++ .github/ISSUE_TEMPLATE/precache-manifest.2a4406da62e0ef82cbd5853b90aa7b74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4198379bede348a9443e2d5875ce872",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99d040dfba027111f089",
+    "url": "/static/css/main.df1aea6f.chunk.css"
+  },
+  {
+    "revision": "f3189d35a877dd68a387",
+    "url": "/static/js/2.90171546.chunk.js"
+  },
+  {
+    "revision": "99d040dfba027111f089",
+    "url": "/static/js/main.d67b8e58.chunk.js"
+  },
+  {
+    "revision": "60f58dc42909bcfbac04",
+    "url": "/static/js/runtime~main.da84151f.js"
+  }
+]);
