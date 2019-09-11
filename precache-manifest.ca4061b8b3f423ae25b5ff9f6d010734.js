9e505df536998d5d8e09d3598a6c27a53b55d01a
krista1 <wf.open@bk.ru>
2019-09-11 17:11:31 +0300

    Add files via upload

---
+++ precache-manifest.ca4061b8b3f423ae25b5ff9f6d010734.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6efd178f3496ad8d6de5075d083d483c",
+    "url": "krista1.github.io/index.html"
+  },
+  {
+    "revision": "7787b6f928160ec6a5e4",
+    "url": "krista1.github.io/static/css/main.df1aea6f.chunk.css"
+  },
+  {
+    "revision": "06a3e7877ab6f4c18a73",
+    "url": "krista1.github.io/static/js/2.0b794f52.chunk.js"
+  },
+  {
+    "revision": "7787b6f928160ec6a5e4",
+    "url": "krista1.github.io/static/js/main.4272ec39.chunk.js"
+  },
+  {
+    "revision": "fe2c80a0edc8cd066879",
+    "url": "krista1.github.io/static/js/runtime~main.00015919.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.69fc3e4bacc447c479ef5641ff0b09c4.js"
+  "krista1.github.io/precache-manifest.ca4061b8b3f423ae25b5ff9f6d010734.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("krista1.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
